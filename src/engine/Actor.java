85b180444aa12a3ef3d4c14a0f2fd51aa6797264
Atul Pradhan <apradhan485@Student.FUHSD.org>
2026-04-10 23:04:54 -0700

    World and Actor class done
    I did the World and Actor class. I watched the video in order to understand the concepts of this lab but my testWorld is not working at the moment because of a class path issue. I will solve this problem and read through the code to understand it.

---
+++ src/engine/Actor.java
@@ -0,0 +1,62 @@
+package engine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.animation.AnimationTimer;
+import javafx.scene.image.ImageView;
+
+public abstract class Actor extends ImageView{
+	
+	public Actor() {
+		
+	}
+	
+	//This method is called every frame once start has been called on the world.
+	abstract void act(long now);
+	//This method is called when an actor is added to the world and should be overridden in subclasses as desired.
+	void addedToWorld() {
+		
+	}
+	//Returns The height of the current image of this actor.
+	public double	getHeight() {
+		return getBoundsInParent().getHeight();
+	}
+	//Returns a list of the actors of a given type intersecting this actor.
+	public <A extends Actor> java.util.List<A>	getIntersectingObjects(java.lang.Class<A> cls){
+		
+		List<A> possibleActors = getWorld().getObjects(cls);
+		List<A> touchingActors = new ArrayList<A>();
+		
+		for(A obj: possibleActors) {
+			if(obj.getBoundsInParent().intersects(this.getBoundsInParent())) {
+				touchingActors.add(obj);
+			}
+		}
+		
+		return touchingActors;
+	}
+	//Returns one actor of the given class that is intersecting this actor.
+	public <A extends Actor> A	getOneIntersectingObject(java.lang.Class<A> cls) {
+		List<A> list = getIntersectingObjects(cls);
+		if(list.size() == 0) {
+			return null;
+		}else {
+			return list.get(0);
+		}
+	}
+	//Returns The width of the current image of this actor.
+	public double	getWidth() {
+		return getBoundsInParent().getHeight();
+	}
+	//returns the world this actor is in, or null if it is not in a world.
+	public World getWorld() {
+		return (World)(getParent());
+	}
+	//Moves this actor by the given dx and dy.
+	public void move(double dx, double dy) {
+		setX(getX() + dx);
+		setY(getY() + dy);
+	}
+	
+}
---
+++ src/engine/World.java
@@ -0,0 +1,176 @@
+package engine;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javafx.animation.AnimationTimer;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
+
+public abstract class World extends Pane{
+	
+	private AnimationTimer timer;
+	private boolean isRunning;
+	private Set<KeyCode> keys;
+	private boolean widthHasBeenSet;
+	private boolean heightHasBeenSet;
+	
+	public World() {
+		widthHasBeenSet = false;
+		heightHasBeenSet = false;
+		keys = new HashSet<KeyCode>();
+		isRunning = false;
+		
+		widthProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+			
+				if(newValue.intValue() > 0) {
+					widthHasBeenSet = true;
+				}
+				if(widthHasBeenSet && heightHasBeenSet) {
+					onDimensionsInitialized();
+				}
+			}
+			
+			
+		});
+		
+		heightProperty().addListener(new ChangeListener<Number>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+			
+				if(newValue.intValue() > 0) {
+					heightHasBeenSet = true;
+				}
+				if(widthHasBeenSet && heightHasBeenSet) {
+					onDimensionsInitialized();
+				}
+			}
+			
+			
+		});
+		
+		sceneProperty().addListener(new ChangeListener<Scene>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Scene> observable, Scene oldValue, Scene newValue) {
+				requestFocus();
+				
+			}
+			
+		});
+		
+		setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+			@Override
+			public void handle(KeyEvent e) {
+				keys.add(e.getCode());
+				
+			}
+			
+		});
+		
+		setOnKeyReleased(new EventHandler<KeyEvent>() {
+
+			@Override
+			public void handle(KeyEvent e) {
+				keys.remove(e.getCode());
+				
+			}
+			
+		});
+		
+		timer = new AnimationTimer() {
+
+			@Override
+			public void handle(long now) {
+				act(now);
+				
+				for(Actor n: getObjects(Actor.class)) {
+					if(n.getWorld() != null) {
+						act(now);
+					}
+				}
+				
+			}
+			
+		};
+		
+	}
+	
+	
+	//This method is called every frame once start has been called.
+	public abstract void act(long now);
+	//Adds the given actor to the world and then calls the addedToWorld() method on the actor that was added.
+	public void add(Actor actor) {
+		getChildren().add(actor);
+	}
+	//Returns a list of all the actors in the world of the given class.
+	 public <A extends Actor> java.util.List<A>	getObjects(java.lang.Class<A> cls){
+		List<A> list = new ArrayList<A>();
+		
+		for(Node n: getChildren()) {
+			if(cls.isInstance(n)) {
+			  list.add(cls.cast(n));
+			}
+		}
+		
+		return list;
+	 }
+	 
+	// Returns a list of all actors of the given class containing the given x, y
+	public <A extends Actor> java.util.List<A>	getObjectsAt(double x, double y, java.lang.Class<A> cls){
+		List<A>  allActors = getObjects(cls);
+		List<A> actors = new ArrayList<A>();
+		
+		
+		for(int i = 0; i < allActors.size(); i++) {
+			if(allActors.get(i).getBoundsInParent().contains(x, y)) {
+				actors.add(allActors.get(i));
+			}
+		}
+	
+		return actors;
+		
+	}
+	//Returns true if the given key is pressed and false otherwise.
+	public boolean	isKeyPressed(javafx.scene.input.KeyCode code) {
+		return false;
+	}
+	//Returns whether or not the world's timer is stopped
+	public boolean	isStopped() {
+		return  isRunning == false;
+	}
+	//Subclasses should override this.
+	public abstract void onDimensionsInitialized();
+	//Removes the given actor from the world.
+	public void remove(Actor actor) {
+		getChildren().remove(actor);
+	}
+	//STARTS the timer that calls the act method on the world and on each Actor in the world each frame.
+	public void start() {
+		timer.start();
+		isRunning = true;
+	}
+	//STOPS the timer that calls the act method on the world and on each Actor in the world each frame.
+	public void stop() {
+		timer.stop();
+		isRunning = false;
+	}
+	
+	
+
+	
+}
+
