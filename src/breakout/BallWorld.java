d43abf47bb8c1c3f473fb517bf5cddd0bc17cec8
Atul Pradhan <apradhan485@Student.FUHSD.org>
2026-04-23 20:54:38 -0700

    Added Mouse and Keyboard Input

--- src/breakout/BallWorld.java
+++ src/breakout/BallWorld.java
@@ -18,6 +18,7 @@ public class BallWorld extends World{
 		});
 		
 		
+		
 	}
 
 	@Override
--- src/breakout/Paddle.java
+++ src/breakout/Paddle.java
@@ -2,10 +2,12 @@ package breakout;
 
 import engine.Actor;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 
 public class Paddle extends Actor{
 	
 	Image image;
+	
 	public Paddle() {
 		String path = getClass().getClassLoader().getResource("breakoutresources/paddle.png").toString();
 		image = new Image(path);
@@ -15,7 +17,12 @@ public class Paddle extends Actor{
 	@Override
 	public void act(long now) {
 		// TODO Auto-generated method stub
-		
+		if(getWorld().isKeyPressed(KeyCode.RIGHT)) {
+			this.setX(this.getX() + 4);
+		}
+		if(getWorld().isKeyPressed(KeyCode.LEFT)) {
+			this.setX(this.getX() - 4);
+		}
 	}
 
 }
